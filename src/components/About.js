9d649261a41fe24379fc116a7c0cd6093eb8fc20
giusmili <giusmili67@gmail.com>
2025-03-26 16:24:33 +0100

    composant fonction

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,34 @@
-// src/components/About.js
-
-import React from 'react';
+import React, { useState } from "react";
 
 const About = () => {
-    return (
-      <section>
-        <h2>About</h2>
+  const [showMore, setShowMore] = useState(false);
+
+  return (
+    <section>
+      <h2>About</h2>
+      <p>
+        At the same time we had reason to hope that these two poor creatures
+        would not see each other again, as the unhappy Ellen never appeared in
+        the daytime, either in the saloons or on the deck.
+      </p>
+
+      {/* Affichage conditionnel du texte supplémentaire */}
+      {
+      showMore && (
         <p>
-        At the same time we had reason to hope that these two poor creatures would not see each other again, as the unhappy Ellen never appeared in the daytime, either in the saloons or on the deck. Only at night, perhaps eluding her gaoler, she came out to bathe herself in the damp air, 
-        and demand of the wind a smooth passage
+          Only at night, perhaps eluding her gaoler, she came out to bathe
+          herself in the damp air, and demand of the wind a smooth passage.
+          as the unhappy Ellen never appeared in
+        the daytime, either in the saloons or on the deck.
         </p>
-      </section>
-    );
-  };
-  
+      )}
+
+      {/* Bouton pour afficher/cacher le texte */}
+      <button onClick={() => setShowMore(!showMore)}>
+        {showMore ? "Lire moins" : "Lire plus"}
+      </button>
+    </section>
+  );
+};
 
 export default About;
